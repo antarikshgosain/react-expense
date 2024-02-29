44fe6c740c44719fb57d4dfc103b660aa4127924
antarikshgosain <antariksh.gosain@gmail.com>
2024-02-28 20:08:38 -0500

    recent expense total styling css added

--- expense-app/components/ExpensesOutput/ExpensesSummary.js
+++ expense-app/components/ExpensesOutput/ExpensesSummary.js
@@ -1,5 +1,5 @@
 import { FlatList, StyleSheet, Text, View } from 'react-native';
-
+import { GlobalStyles } from '../../constants/styles';
 
 function ExpensesSummary({periodName, expenses}) {
 
@@ -7,11 +7,33 @@ function ExpensesSummary({periodName, expenses}) {
     //reduce method will give total by adding all expense.amount to variable 'sum' (initially zero)
 
     return (
-        <View>
-            <Text>{periodName}</Text>
-            <Text>$ {expensesSum.toFixed(2)}</Text>
+        <View style={styles.container}>
+            <Text style={styles.period}>{periodName}</Text>
+            <Text style={styles.sum}>$ {expensesSum.toFixed(2)}</Text>
         </View>
     );
 }
 
 export default ExpensesSummary;
+
+
+const styles = StyleSheet.create({
+    container: {
+        padding: 10,
+        backgroundColor: GlobalStyles.colors.primary50,
+        borderRadius: 8,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center'
+    },
+    period: {
+        fontSize: 12,
+        color: GlobalStyles.colors.primary400,
+
+    },
+    sum:{
+        fontWeight: 20,
+        fontWeight: 'bold',
+        color: GlobalStyles.colors.primary500
+    },
+});
