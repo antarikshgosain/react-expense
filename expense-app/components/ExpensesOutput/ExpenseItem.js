0e4562e226e8edc16fcb3db936de8d08f098942c
antarikshgosain <antariksh.gosain@gmail.com>
2024-03-01 18:49:45 -0500

    expenses item design upgraded

--- expense-app/components/ExpensesOutput/ExpenseItem.js
+++ expense-app/components/ExpensesOutput/ExpenseItem.js
@@ -1,5 +1,6 @@
 import { FlatList, Pressable, StyleSheet, Text, View } from 'react-native';
 import { GlobalStyles } from '../../constants/styles';
+import { getFormattedDate } from '../../util/Date';
 
 
 function ExpensesItem({description, amount, date}) {
@@ -8,10 +9,10 @@ function ExpensesItem({description, amount, date}) {
             <View style={styles.expenseItem}>
                 <View>
                     <Text style={[styles.textBase, styles.description]}>{description}</Text>
-                    <Text style={[styles.textBase]}>{date.toString()}</Text>
+                    <Text style={[styles.textBase]}>{getFormattedDate(date)}</Text>
                 </View>
                 <View style={styles.amountContainer}>
-                    <Text>{amount}</Text>
+                    <Text>{amount.toFixed(2)}</Text>
                 </View>
             </View>
         </Pressable>
@@ -54,6 +55,8 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
         borderRadius: 2,
+        minWidth: 80,
+        maxWidth: 80
     },
     amount:{
         color: GlobalStyles.colors.primary500,
